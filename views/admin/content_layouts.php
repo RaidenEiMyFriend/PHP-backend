cf643616a26f901b54eef64852a33b73f62e08a4
hoz12 <137584722+RaidenEiMyFriend@users.noreply.github.com>
2023-11-25 12:22:42 +0700

    Update content_layouts.php

--- views/admin/content_layouts.php
+++ views/admin/content_layouts.php
@@ -8,7 +8,7 @@
 		</button>
 		<div class="navbar-nav">
 			<div class="nav-item text-nowrap">
-				<a class="nav-link px-3" href="#" id="title">Admintrator</a>
+				<a class="nav-link px-3" href="#" id="title">Administrator</a>
 			</div>
 		</div>
 		<div class="navbar-nav">
